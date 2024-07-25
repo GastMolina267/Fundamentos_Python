0b2351586994633ec62e3c20213c4a0ee520b17c
Gaston <gastonmolina267@gmail.com>
2024-07-25 17:10:23 -0300

    Nuevo_Archivo_05-Listas

---
+++ 2_Estructuras_de_Datos/05_Listas.py
@@ -0,0 +1,36 @@
+### Listas ###
+
+# Uso de la función incorporada de lista
+empty_lst = list()
+print(len(empty_lst))
+
+#Usando corchetes []
+empty_lst = []
+print(len(empty_lst))
+
+# Listas con valores iniciales
+fruits = ['banana', 'orange', 'mango', 'lemon']                     # list of fruits
+vegetables = ['Tomato', 'Potato', 'Cabbage','Onion', 'Carrot']      # list of vegetables
+animal_products = ['milk', 'meat', 'butter', 'yoghurt']             # list of animal products
+web_techs = ['HTML', 'CSS', 'JS', 'React','Redux', 'Node', 'MongDB'] # list of web technologies
+countries = ['Finland', 'Estonia', 'Denmark', 'Sweden', 'Norway']
+
+# Print the lists and its length
+print('Fruits:', fruits)
+print('Number of fruits:', len(fruits))
+print('Vegetables:', vegetables)
+print('Number of vegetables:', len(vegetables))
+print('Animal products:',animal_products)
+print('Number of animal products:', len(animal_products))
+print('Web technologies:', web_techs)
+print('Number of web technologies:', len(web_techs))
+print('Countries:', countries)
+print('Number of countries:', len(countries))
+
+# Desembalaje de elementos de la lista
+lst = ['item1','item2','item3', 'item4', 'item5']
+first_item, second_item, third_item, *rest = lst
+print(first_item)     # item1
+print(second_item)    # item2
+print(third_item)     # item3
+print(rest)           # ['item4', 'item5']
