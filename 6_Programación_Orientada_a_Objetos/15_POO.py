943f3aeba49309d8d7b80d5bf03aeaf98958227c
Gaston <gastonmolina267@gmail.com>
2024-07-31 18:48:04 -0300

    Agregando_Principios_POO

--- 6_Programación_Orientada_a_Objetos/15_POO.py
+++ 6_Programación_Orientada_a_Objetos/15_POO.py
@@ -114,3 +114,128 @@ p2 = Person('John', 'Doe', 30, 'Nomanland', 'Noman city')
 print(p2.person_info())
 print(p1.skills)
 print(p2.skills)
+
+## Encapsulamiento
+'''
+Permite restringir el acceso a ciertos componentes del objeto, protegiendo los datos y asegurando que se manipulen de manera controlada
+'''
+# Atributos Públicos
+'Accesibles desde cualquier parte del programa'
+class Person:
+      def __init__(self, firstname='Marco', lastname='Aurelio', age=250, country='Finland', city='Helsinki'):
+          self.firstname = firstname # <--- Para que sea público, no se le agrega nada al atributo
+          self.lastname = lastname
+          self.age = age
+          self.country = country
+          self.city = city
+p1 = Person()
+print(p1.firstname) # Output: Marco
+
+# Atributos Privados
+'Sólo son accesibles dentro de la propia clase'
+class Person:
+      def __init__(self, firstname='Marco', lastname='Aurelio', age=250, country='Finland', city='Helsinki'):
+          self.__firstname = firstname # <--- Para que sea privado, se le agrega __ al atributo
+          self.__lastname = lastname
+          self.__age = age
+          self.__country = country
+          self.__city = city
+
+p2 = Person()
+print(p2.__firstname) # Output: Error
+
+## Acceso a los atributos privados. Métodos Getter y Setter
+class Player:
+    def __init__(self, name, ability, level, health, mana):
+        self.__name = name
+        self.__ability = ability
+        self.__level = level
+        self.__health = health
+        self.__mana = mana
+# Getter
+'''
+Un método que permite acceder a un atributo privado desde fuera de la clase
+Se utiliza para obtener el valor de un atributo privado de manera controlada
+'''
+def get_name(self):
+    return self.__name
+
+def get_ability(self):
+    return self.__ability
+
+def get_level(self):
+    return self.__level
+
+def get_health(self):
+    return self.__health
+
+def get_mana(self):
+    return self.__mana
+# Setter
+'''
+Un método que permite modificar un atributo privado desde fuera de la clase
+Se utiliza para establecer o cambiar el valor de un atributo privado de manera controlada
+'''
+def set_level(self, new_level):
+    self.__level = new_level
+
+p3 = Player("Marco","philosopher", 50, 100, 0)
+print(p3.get_name)
+print(p3.set_level(102))
+
+## Herencia
+'''
+Permite crear una nueva clase a partir de una clase existente.
+La nueva clase, llamada clase derivada o subclase, hereda los atributos y métodos de la clase base o superclase
+Permite reutilizar el código de una clase ya existente
+'''
+class Player: # <--- Clase Base 
+    def __init__(self, name, ability, level, health, mana):
+        self.__name = name
+        self.__ability = ability
+        self.__level = level
+        self.__health = health
+        self.__mana = mana
+
+class Magician(Player): # <--- Clase Heredada
+    def __init__(self, name, ability, level, health, mana, magic_power):
+        super().__init__(name, ability, level, health, mana)
+        self.magic_power = magic_power
+
+
+## Polimorfismo
+'''
+Permite que objetos de diferentes clases, sean tratados como de una clase común.
+Esto se logra mediante la herencia y la sobreescritura de métodos.
+El polimorfismo permite utilizar una misma interfaz para diferentes tipos de objetos.
+'''
+class Player: # <--- Clase Base 
+    def __init__(self, name, ability, level, health, mana):
+        self.__name = name
+        self.__ability = ability
+        self.__level = level
+        self.__health = health
+        self.__mana = mana
+    def attack(self, enemy):
+        f"{self.__name} ataca a {enemy}"
+
+class Magician(Player): # <--- Clase Heredada
+    def __init__(self, name, ability, level, health, mana, magic_power):
+        super().__init__(name, ability, level, health, mana)
+        self.magic_power = magic_power
+    def attack(self, enemy):
+        return f"{self.__name} invoca un poder desconocido hacia {enemy}"
+p5 = Magician("Gandalf", 100, 50, 20, 250, 500)
+
+class Warrior(Player): # <--- Clase Heredada
+    def __init__(self, name, ability, level, health, mana, force):
+        super().__init__(name, ability, level, health, mana)
+        self.force = force
+    def attack(self, enemy):
+        return f"{self.__name} arremete contra {enemy}"
+
+p6 = Warrior("Leonidas", 120, 42, 50, 10, 500)
+
+def stroke(player, enemy):
+    print("Te lanzaré mi mejor ataque")
+    print(player.attack(enemy))
