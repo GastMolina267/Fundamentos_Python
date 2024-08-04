7d54edd53d0ae3ca7a6eafeaea7efd8b914dca98
Gaston <gastonmolina267@gmail.com>
2024-08-03 21:36:29 -0300

    Agregando_21_Imágnes

--- 7_Tkinter/21_Tkinter_Gestión_Imágenes.py
+++ 7_Tkinter/21_Tkinter_Gestión_Imágenes.py
@@ -1,16 +1,27 @@
 ### TKINTER ###
 ## Gestión de archivos
 import tkinter as tk
-# Nos permite trabajar con imágenes
+# Nos permite trabajar con imágenes más allá de la función que viene integrada con Python
+# PIL permite manipular mejor las imágenes y trabajar con archivos jpg
 from PIL import Image, ImageTk
+import os
+
+# Verifica si el archivo de imagen existe en el directorio actual
+if not os.path.exists("./Media/apple.png"):
+    raise FileNotFoundError("No se encuentra el archivo 'apple.png' en el directorio actual.")
 
 ventana = tk.Tk()
-etiqueta = tk.Label(ventana)
+ventana.title("Gestión de Imágenes con Tkinter")
 
-imagen = Image.open("apple.png")
-imagen_tk = ImageTk.PhotoImage(imagen)
+# Cargar la imagen
+imagen = Image.open("./Media/apple.png")
+imagen_redimensionada = imagen.resize((50,50)) # Redimensionar la imágen en px
+imagen_rotada = imagen_redimensionada.rotate(90) # Rotar la imágen
+imagen_tk = ImageTk.PhotoImage(imagen_rotada) # Establecer la imágen
 
-boton = tk.Button(ventana, image=imagen_tk)
+# Crear y mostrar el botón con la imagen
+boton = tk.Button(ventana, image=imagen_tk, padx=10, pady=10)
 boton.pack()
 
+# Ejecutar la ventana principal de la aplicación
 ventana.mainloop()
