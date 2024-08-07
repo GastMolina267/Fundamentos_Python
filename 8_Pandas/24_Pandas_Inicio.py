21c3eb332286cb43a95bc4e28e4f723ff255d4cd
Gaston <gastonmolina267@gmail.com>
2024-08-07 12:37:51 -0300

    Comenzando con Pandas

--- 8_Pandas/24_Pandas_Inicio.py
+++ 8_Pandas/24_Pandas_Inicio.py
@@ -1,1 +1,19 @@
 ### PANDAS ###
+## Permite manejar y modificar datos de manera eficiente 
+# Es perfecto para manejar datos tabulares, es decir que se puedan organizar en una tabla
+
+import pandas as pd
+
+serie = pd.Series([1,2,3,4,5,6,7,8,9,10]) # Es como una columna en Excel
+print(serie)
+
+datos = {
+    'nombre' : ["Gaston", "Edgar", "Tomas", "Facundo", "Manue", "Gonza"],
+    'edades' :  [23, 24, 25, 28, 29, 30]
+}
+
+df = pd.DataFrame(datos) 
+# Creación de un DataFrame
+# Un DataFrame es una serie de Series Pandas indexadas por un valor
+# o bien es una colección de datos habitualmente tabulada
+print(df)
---
+++ 8_Pandas/25_Pandas_AbrirCSV.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+df = pd.read_csv("./Media/data.csv")
+'''
+    Si nos diese un error porque el archivo tiene una codificación utf-8
+    agregar read_csv("ruta del archivo csv", encoding="ISO-8859-1")
+'''
+print(df.columns)
+print(df.head())
+# La función head() nos muestra los primeros 5 datos
+
+print(df.shape)
+# La función shape nos muestra la cantidad de filas y columnas que tiene el archivo
+
+df_ordenado = df.sort_values('Maxpulse')
+# La función sort_values() ordena de menor a mayor (en caso de valores numéricos) y en orden alfabético (en caso de nombres)
+print(df_ordenado.head())
+
+df_filtrado = df[df['Maxpulse'] > 150]
+# Es una de acceder a cierta columna y de filtrar que tipo de datos quiero que se muestren
+print(df_filtrado)
