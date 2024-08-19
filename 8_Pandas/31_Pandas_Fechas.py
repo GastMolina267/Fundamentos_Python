2d2c960149966d125d7f5d70d4de72b9edf01b10
Gaston <gastonmolina267@gmail.com>
2024-08-19 12:27:51 -0300

    Agregando_Traza_serieTiempo

--- 8_Pandas/31_Pandas_Fechas.py
+++ 8_Pandas/31_Pandas_Fechas.py
@@ -1,13 +1,64 @@
-### PANDAS ###
-## Trabajando con fechas de DATOS
 from datetime import datetime
 import pandas as pd
 import numpy as np
 
-##  Instalar Jupyter Notebook
-
+# %%
 # Convertir cadena de texto a datatime
-fecha_str = '2024-13-08 9:30:00:00'
+fecha_str = '2024-08-13 09:30:00'
 fecha_dt = pd.to_datetime(fecha_str)
-print(type(fecha_dt))
-print(type(fecha_str))
+## Objetos diferentes
+print(fecha_dt)
+print(fecha_str)
+
+# %%
+# Creando un objeto Timestamp en Pandas
+timestap = pd.Timestamp(datetime.now())
+print(timestap)
+
+# %%
+# Creando un DatatimeIndex en Pandas
+meses = pd.date_range(start='2024-01-01', end='2024-12-31', freq='M')
+print(meses)
+
+# %%
+dias = pd.date_range(start='2024-01-01', end='2024-01-31', freq='D')
+print(dias)
+
+# %%
+dias = pd.date_range(start='2024-01-01', end='2024-01-31', freq='H')
+print(dias)
+
+# %%
+# Creación de series de tiempo
+serie = pd.Series(np.random.rand(len(meses)), index=meses)
+print(serie)
+
+# %%
+from datetime import timedelta
+# Creando un objeto timedelta
+# Creando un objeto Timestamp en Pandas
+timestap = pd.Timestamp(datetime.now())
+print(timestap)
+delta = timedelta(days=7)
+print(delta)
+nueva_fecha = timestap + delta
+print(nueva_fecha)
+
+# %%
+# Suponiendo que 'precios' es nuestra serie de tiempo de precios de cierre diarios
+precios = pd.Series(np.random.rand(100), index=pd.date_range(start='2024-01-01', periods=100, freq='D'))
+print(precios)
+# Calcular la media móvil de los últimos 7 días
+media_movil = precios.rolling(window=7).mean()
+print(media_movil.head(7))
+
+# %%
+import matplotlib.pyplot as plt
+
+# Trazar la serie de tiempo de precios y la media móvil
+plt.figure(figsize=(12,6))
+precios.plot(label='Precio de cierre')
+media_movil.plot(label='Media móvil de 7 días')
+plt.title('Precio de cierre y media móvil de 7 días')
+plt.legend()
+plt.show()
